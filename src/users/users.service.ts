97395797bd65191d0486a81a30001e70b545be76
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2024-09-17 21:02:49 +0300

    Added authentication

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -9,6 +11,22 @@ export class UsersService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+
+
+  async create(createUserDto: CreateUserDto): Promise<null> {
+    const userExists = await this.findByEmail(createUserDto.email);
+    if (userExists) throw new ConflictException('User with this email already exists');
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const user = this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    await this.usersRepository.save(user);
+    return null;
+  }
 
 
   async findByEmail(email: string) {
