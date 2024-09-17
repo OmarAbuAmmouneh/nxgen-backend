a72f49f1c4d62f8a8966141985f43517dab41517
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2024-09-17 18:51:42 +0300

    Added jobs apis

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -17,10 +17,10 @@ export class JobsController {
     return this.jobsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.jobsService.findOne(+id);
+  // }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { Job } from './entities/job.entity';
 
 @Injectable()
 export class JobsService {
-  create(createJobDto: CreateJobDto) {
-    return 'This action adds a new job';
+  constructor(
+    @InjectRepository(Job)
+    private jobsRepository: Repository<Job>,
+  ) {}
+
+  async create(createJobDto: CreateJobDto): Promise<Job> {
+    const job = this.jobsRepository.create(createJobDto);
+    return await this.jobsRepository.save(job);
   }
 
-  findAll() {
-    return `This action returns all jobs`;
+  async update(id: number, updateJobDto: UpdateJobDto): Promise<Job> {
+    const job = await this.jobsRepository.preload({ id: id, ...updateJobDto });
+    if (!job) {
+      throw new NotFoundException(`Job with ID ${id} not found`);
+    }
+    return this.jobsRepository.save(job);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  async remove(id: number): Promise<void> {
+    const job = await this.jobsRepository.findOneBy({ id: id });
+    if (!job) {
+      throw new NotFoundException(`Job with ID ${id} not found`);
+    }
+    await this.jobsRepository.remove(job);
   }
 
-  update(id: number, updateJobDto: UpdateJobDto) {
-    return `This action updates a #${id} job`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} job`;
+  findAll(): Promise<Job[]> {
+    return this.jobsRepository.find();
   }
 }
